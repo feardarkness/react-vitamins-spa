726a3b4c49befc2506ff5ef97ecefad8d8b52e05
feardarkness <alvarado.ariel.contact@gmail.com>
2019-02-08 16:09:08 -0400

    Added vitamins list

--- src/components/vitamins/Vitamins.js
+++ src/components/vitamins/Vitamins.js
@@ -14,20 +14,37 @@ class Vitamins extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://127.0.0.1:6060/vitamins?resourceName=Calcium&readingLevel=Health Professional')
+    axios.get('http://127.0.0.1:6060/vitamins')
       .then((result) => {
         this.setState({
           isLoaded: true,
           vitamins: result.data.vitamins,
         });
       })
-      .catch(console.log);
+      .catch(err => {
+        this.setState({
+          isLoaded: true,
+          error: err,
+        });
+      });
   }
 
   render() {
+    const { isLoaded, error, vitamins } = this.state;
+    if (!isLoaded) {
+      return (
+        <div><h2>Loading...</h2></div>
+      );
+    } else if (error) {
+      return (
+        <div>
+          <h2 className="error">Some error happened // add some error here based on the API response</h2>
+        </div>
+      );
+    }
     return (
       <div>
-        <VitaminsList></VitaminsList>
+        <VitaminsList vitamins={vitamins}></VitaminsList>
         <VitaminsDetail></VitaminsDetail>
       </div>
     );
--- src/components/vitamins/VitaminsList.js
+++ src/components/vitamins/VitaminsList.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react'
 
 class VitaminsList extends Component {
   render() {
+    const { vitamins } = this.props;
     return (
       <ul>
-        <li><a href="#el1">Element 1</a></li>
-        <li><a href="#el2">Element 2</a></li>
-        <li><a href="#el3">Element 3</a></li>
-        <li><a href="#el4">Element 4</a></li>
+        {vitamins.map(vitamin => <li key={vitamin}><a href={`#${vitamin}`}>{vitamin}</a></li>)}
       </ul>
     );
   }
